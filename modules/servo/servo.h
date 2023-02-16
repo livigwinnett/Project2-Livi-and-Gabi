dafcd3aeb68b237cd01a31a5fe24988ddc1c39cc
livigwinnett <124712098+livigwinnett@users.noreply.github.com>
2023-02-16 15:50:29 -0500

    Create servo.h

---
+++ modules/servo/servo.h
@@ -0,0 +1,18 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SERVO_MOTOR_H_
+#define _SERVO_MOTOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void gateInit();
+void gateOpen();
+void gateClose();
+
+//=====[#include guards - end]=================================================
+
+#endif // _SERVO_MOTOR_H_
